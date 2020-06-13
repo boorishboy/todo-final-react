ac81c095933a47dd8bbce0fb325936c330703e5a
boorishboy <wojciech.chamski@gmail.com>
2020-06-13 15:53:03 +0200

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function header(){
+    return (
+        <div className="header">
+            <h3>HERE SHOULD BE AN IMAGE BUT IT REQUIRES STACKBLITZ PREMIUM</h3>
+            <h1 className="display-1">React Final Project</h1>
+        </div>
+    )
+}
+
+export default header;
+
